5648b3d8b6372597b0276736cdb9a04857439dca
Zijia Ke <kevinkozj@gmail.com>
2017-04-21 10:44:16 -0700

    Tiny updates on the front-end upload button
    Delete the code in onUpload() to remove the error when start the
    Angular2.

--- textdb/textdb-angular-gui/app/side-bar.component.ts
+++ textdb/textdb-angular-gui/app/side-bar.component.ts
@@ -111,22 +111,7 @@ export class SideBarComponent {
     // refer to http://stackoverflow.com/questions/40214772/file-upload-in-angular-2
 
     onUpload(event) {
-        let fileList: FileList = event.target.files;
-        if(fileList.length > 0) {
-            let file: File = fileList[0];
-            let formData:FormData = new FormData();
-            formData.append('uploadFile', file, file.name);
-            let headers = new Headers();
-            headers.append('Content-Type', 'multipart/form-data');
-            headers.append('Accept', 'application/json');
-            let options = new RequestOptions({ headers: headers });
-            this.http.post(`${this.apiEndPoint}`, formData, options)
-                .map(res => res.json())
-                .catch(error => Observable.throw(error))
-                .subscribe(
-                    data => console.log('success'),
-                    error => console.log(error))
-        }
+
     }
 
     onAddDictionary() {
